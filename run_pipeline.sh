9c5f027f57fad9bdee0f25158b826e3bcc6095fa
VainlyStrain <51248447+VainlyStrain@users.noreply.github.com>
2024-11-11 18:00:42 +0100

    Update run_pipeline.sh
    Set required environment variables for the pipeline process to prevent Neo4J initialization issues, in case the user rebooted after running install.sh and didn't add export statements to their ~/.bashrc.

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-screen -dmS s1 bash -c 'python3 -m run_pipeline --conf=config.yaml; exec sh'
+INEO_HOME=$(pwd)/ineo/ PATH=$INEO_HOME/bin:$PATH screen -dmS s1 bash -c 'python3 -m run_pipeline --conf=config.yaml; exec sh'
